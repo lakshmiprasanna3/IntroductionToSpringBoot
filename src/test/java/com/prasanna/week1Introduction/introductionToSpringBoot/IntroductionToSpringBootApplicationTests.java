89764402be7f6f39eb3d7ae05f148aebc408855c
coffeecodes33 <programmerplanet33@gmail.com>
2024-07-03 07:57:05 +0530

    first commit

---
+++ src/test/java/com/prasanna/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasanna.week1Introduction.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
